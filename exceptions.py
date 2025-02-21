d998510d07b6441514bff3880c5c04067026995c
Дмитрий Демьянов <dmeetrogon@gmail.com>
2025-02-21 16:01:16 +0300

    reorganized classes and .py files

--- exceptions.py
+++
@@ -1,6 +0,0 @@
-class HostAlreadyExistException(Exception):
-    def __init__(self, message):
-        super().__init__(message)
-class ClientNotFoundException(Exception):
-    def __init__(self, message):
-        super().__init__(message)

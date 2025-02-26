9d514bc5f1d9f6269029fa621ee8f66f2d7ceff7
Дмитрий Демьянов <dmeetrogon@gmail.com>
2025-02-26 12:57:24 +0300

    edit pyproject.toml and update __init__.py

--- src/py_multi_3x_ui/__init__.py
+++ src/py_multi_3x_ui/__init__.py
@@ -1,1 +1,5 @@
-from py_multi_3x_ui.server.server import Server
+from server.server import Server
+from managers.server_data_manager import ServerDataManager
+from managers.connections_manager import ConnectionsManager
+from exceptions.exceptions import ClientNotFoundException,HostAlreadyExistException
+

37ef1d54622a22aead8f09927954db44b38c5141
Дмитрий Демьянов <dmeetrogon@gmail.com>
2025-03-03 19:13:33 +0300

    edit __init__.py files and pyproject.toml for package distribution

--- src/py_multi_3x_ui/__init__.py
+++
@@ -1,5 +0,0 @@
-from server.server import Server
-from managers.server_data_manager import ServerDataManager
-from managers.connections_manager import ConnectionsManager
-from exceptions.exceptions import ClientNotFoundException,HostAlreadyExistException
-

75e9066396fb13b49d8a1fc4c3988ef23c62d552
Дмитрий Демьянов <dmeetrogon@gmail.com>
2025-08-08 18:08:32 +0300

    edit AuthCookieManager

--- py_multi_3xui/managers/auth_cookie_manager.py
+++ py_multi_3xui/managers/auth_cookie_manager.py
@@ -1,9 +1,13 @@
 import time
 import diskcache as dc
+import pyotp
+import requests
 
 from py3xui import Api
 import logging
 logger = logging.getLogger(__name__)
+
+cache_path = "/temp/cookie_cache"
 
 class AuthCookieManager:
     @staticmethod
@@ -18,21 +22,28 @@ class AuthCookieManager:
         password = server_dict["password"]
         admin_username = server_dict["admin_username"]
         use_tls_verification = bool(server_dict["use_tls_verification"])
-
-        cache = dc.Cache("/temp/cookie_cache")
+        secret_token_for_2FA = server_dict["secret_token_for_2FA"]
+        cache = dc.Cache(cache_path)
         cached = cache.get(host)
         if cached:
             age = time.time() - cached["created_at"]
             if age < 3600:
-                logger.debug("Got cookie from memory")
-                return cached["value"]
-        logger.debug("cookie was too old or it doesnt exist. creating new one.")
+                cookie = cached["value"]
+                if AuthCookieManager.check_auth_cookie(session=cookie,
+                                                       host=host):
+                    logger.debug("Got cookie from memory")
+                    return cookie
+
+
+
+        logger.debug("cookie was old/incorrect. creating new one.")
         connection = Api(host=host,
                          password=password,
                          username=admin_username,
                          use_tls_verify=use_tls_verification)
         created_at = time.time()
-        connection.login()
+        totp = pyotp.TOTP(secret_token_for_2FA)
+        connection.login(totp.now())
         logger.debug("new cookie acquired")
         new_cookie = {
             "value":connection.session,
@@ -41,7 +52,33 @@ class AuthCookieManager:
         cache.set(host,new_cookie,expire=3600)
         logger.info(f"updated cookie for {server_dict["host"]}")
         return new_cookie["value"]
-
-
-
-
+    @staticmethod
+    def check_auth_cookie(session:str,host:str):
+        url = f"{host}/api/inbounds/list"
+        headers = {
+            'Cookie': f'session={session}'
+        }
+        try:
+            response = requests.get(url, headers=headers)
+            if response.status_code == 200:
+                return True
+            elif response.status_code == 401:
+                return False
+            else:
+                return False
+        except requests.exceptions.RequestException as e:
+            return False
+    @staticmethod
+    def clear_all_cookies():
+        """
+       Delete ALL cached cookie.
+        """
+        with dc.Cache(cache_path) as cache:
+            cache.clear()
+    @staticmethod
+    def delete_cookie_by_host(host: str):
+        """
+        delete cookie for one server.
+        """
+        with dc.Cache(cache_path) as cache:
+            cache.delete(host)
